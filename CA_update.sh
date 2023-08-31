7eab8014ff4aea15c07ea3054d42278503b465f4
GR0B <grobschmit@gmail.com>
2023-08-31 15:24:40 +1000

    Update CA_update.sh
    -Changed the checking process to give less errors if CA cert is missing, as we know it is. 
    -Added a little more output info

--- CA_update.sh
+++ CA_update.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # 2023-08-31 Robert Sturzbecher
-# This script fixes the issue of Debian Jessie giving a SSL expired error caused by not having the CA cert installed. 
+# This script fixes the issue of Debian Jessie giving a SSL expired error caused by not having the CA cert installed.
 # But you know the correct fix is really to upgrade from Jessie ;)
 
-ISRG_CHECK=$( openssl x509 -noout -text -in /usr/share/ca-certificates/mozilla/ISRG_Root_X1.crt | grep Issuer | grep "CN = ISRG Root X1" )
-
-if [[ ! $ISRG_CHECK ]]; then
-  curl -o /usr/share/ca-certificates/mozilla/ISRG_Root_X1.crt https://letsencrypt.org/certs/isrgrootx1.pem -k
-  sed -i 's/mozilla\/DST_Root_CA_X3.crt/!mozilla\/DST_Root_CA_X3.crt/g' /etc/ca-certificates.conf
-  echo "mozilla/ISRG_Root_X1.crt" | tee -a /etc/ca-certificates.conf
-  update-ca-certificates
+if [[ ! -f "/usr/share/ca-certificates/mozilla/ISRG_Root_X1.crt" ]]; then
+    echo CA Cert missing, installing...
+    curl -o /usr/share/ca-certificates/mozilla/ISRG_Root_X1.crt https://letsencrypt.org/certs/isrgrootx1.pem -k
+    sed -i 's/mozilla\/DST_Root_CA_X3.crt/!mozilla\/DST_Root_CA_X3.crt/g' /etc/ca-certificates.conf
+    echo "mozilla/ISRG_Root_X1.crt" | tee -a /etc/ca-certificates.conf
+    update-ca-certificates
+else
+    echo CA cert already present, no changes made.
 fi
